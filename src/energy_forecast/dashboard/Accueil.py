07da0a7448733ae6fbb9ddbed5204a8872038196
Antoine TAVANT <antoine.tavant@ecoco2.com>
2024-07-02 11:46:28 +0000

    Fix again the links

--- src/energy_forecast/dashboard/Accueil.py
+++ src/energy_forecast/dashboard/Accueil.py
@@ -12,10 +12,10 @@ Cette application est une démonstration d'un tableau de bord de prévision éne
 
 Il est divisé en trois sections:
 
-- <a href="1_Météo" target = "_self">Prévision météo</a>
-- <a href="2_Production" target = "_self">Prévision énergétique</a>
-- <a href="3_Consommation" target = "_self">Prévision de consommation</a>
-- <a href="4_Jours_Tempos" target = "_self">Prédiction Tempo</a>
+- <a href="Météo" target = "_self">Prévision météo</a>
+- <a href="Production" target = "_self">Prévision énergétique</a>
+- <a href="Consommation" target = "_self">Prévision de consommation</a>
+- <a href="Jours_Tempos" target = "_self">Prédiction Tempo</a>
 """
     , unsafe_allow_html=True)
 
