66e60c1e0a36f5c6eec86367b03e4e5a64c5d054
Antoine Tavant <antoinetavant@hotmail.fr>
2024-09-30 10:24:06 +0200

    Update the Streamlit App

--- src/energy_forecast/dashboard/Accueil.py
+++ src/energy_forecast/dashboard/Accueil.py
@@ -1,5 +1,43 @@
 """Landing page for the Energy Forecast dashboard."""
 import streamlit as st
+import streamlit as st
+import pandas as pd
+from energy_forecast import ROOT_DIR
+import locale
+import numpy as np
+
+try :
+    locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
+except locale.Error:
+    logger.warning("Could not set locale to fr_FR.UTF-8")
+    
+
+gold_dir = ROOT_DIR / "data" / "gold"
+tempo_prediction_file = gold_dir / "our_tempo_prediction.csv"
+
+background_colors = {
+    "Blanc": "white",
+    "Rouge": "red",
+    "Bleu": "blue",
+    "Inconnu": "silver",
+}
+text_colors = {
+    "Blanc": "black",
+    "Rouge": "white",
+    "Bleu": "white",
+}
+
+map_rte_signal_to_text = {
+    "WHITE": "Blanc",
+    "RED": "Rouge",
+    "BLUE": "Bleu",
+    np.nan: "Inconnu",
+}
+map_our_signal_to_text = {
+    "prediction_blanc": "Blanc",
+    "prediction_rouge": "Rouge",
+    "prediction_bleu": "Bleu",
+}
 
 if __name__ == "__main__":
     st.set_page_config(
@@ -7,16 +45,48 @@ if __name__ == "__main__":
         page_icon="🌞",
     )
 
-    st.markdown("""# Bienvenue dans Energy Forecast! 🌞
-Cette application est une démonstration d'un tableau de bord de prévision énergétique simple.
+
+    
+        
+    st.title("🌞Calendier des jours Tempo❄️")
+    st.markdown("Voici le calendrier des jours Tempo prédits par notre modèle et les jours Tempo réels selectionés par RTE.")
+    today = pd.Timestamp.now().floor("D")
+    predictions = pd.read_csv(tempo_prediction_file, index_col=0, parse_dates=True)[today:]
+    predictions.index = pd.to_datetime(predictions.index, utc=True)
+    len_predictions = len(predictions)
+    cols = st.columns(len_predictions)
+
+    # Create a table with two rows: one for our predictions and one for the true values
+    st.write("### Tableau des prévisions et des valeurs réelles")
+
+    # Create a DataFrame to hold the data for the table
+    table_data = {
+        "Date": predictions.index.strftime("%a %d %B %Y"),
+        "Notre prévision": [map_our_signal_to_text[pred] for pred in predictions["our_tempo"]],
+        "Valeur réelle": [map_rte_signal_to_text[true_val] for true_val in predictions["Type_de_jour_TEMPO"]],
+    }
+
+    table_df = pd.DataFrame(table_data).set_index("Date").T
+
+    # Apply background colors to the table cells based on the content
+    def apply_background_color(val):
+        color = background_colors.get(val, "white")
+        return f'background-color: {color}; color: {text_colors.get(val, "black")}'
+
+    # Apply the background color function to the DataFrame
+    styled_table_df = table_df.style.map(apply_background_color)
+
+    # Display the styled table
+    st.write(styled_table_df.to_html(), unsafe_allow_html=True)
+    
+    st.markdown("""## Comment ça marche?
+Cette application est une démonstration d'une prévision énergétique simple.
 
 Il est divisé en trois sections:
 
-- <a href="Météo" target = "_self">Prévision météo</a>
-- <a href="Production" target = "_self">Prévision énergétique</a>
-- <a href="Consommation" target = "_self">Prévision de consommation</a>
-- <a href="Jours_Tempos" target = "_self">Prédiction Tempo</a>
+- <a href="Météo" target = "_self">Prévision météo</a> : analyse les prévision météo
+- <a href="Production" target = "_self">Prévision de production électrique</a>: utilise la prévision météo pour estimer la production éolienne et solaire
+- <a href="Consommation" target = "_self">Prévision de consommation</a>: récupère les données de consommation prévue par RTE
+- Une fois la prévision de production et de consommation obtenue, la <a href="#2b45dae7" target = "_self">prévision Tempo</a> est calculée.
 """
     , unsafe_allow_html=True)
-
-    st.sidebar.success("Sélectionnez une page ci-dessus.")
--- src/energy_forecast/dashboard/pages/1_Météo.py
+++ src/energy_forecast/dashboard/pages/1_Météo.py
@@ -1,9 +1,16 @@
 """This Page is used to display the weather forecast data."""
-from energy_forecast.meteo import get_region_sun, get_region_wind, memory
+from matplotlib import pyplot as plt
+from energy_forecast.meteo import get_region_sun, get_region_wind, memory, ArpegeSimpleAPI
+from energy_forecast.constants import france_bounds
 import pandas as pd
 import streamlit as st
 import altair as alt
-alt.renderers.set_embed_options(time_format_locale="fr-FR", format_locale="fr-FR")
+import locale
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import xarray as xr
+locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
+alt.renderers.set_embed_options(time_format_locale="fr-FR.UTF-8", format_locale="fr-FR.UTF-8")
 
 
 @memory.cache
@@ -91,6 +98,60 @@ def compute_data_wind(date: str)->pd.DataFrame:
     data.columns = ["time", "wind_speed"]
     return data
 
+@memory.cache
+def get_noon_wind_map(date: str, step="12h")->xr.DataArray:
+    """Return the mean wind speed at noon for a given date.
+
+    Parameters
+    ----------
+    date : str
+        the date at which the weather forecast is requested.
+        Must be a valid date format, e.g. ``"YYYY-MM-DD"``
+        or a ``datetime.date`` object.
+
+    Returns
+    -------
+    float
+        The mean wind speed at noon.
+
+    Examples
+    --------
+    >>> get_noon_wind("2024-06-28")
+    2.0
+
+    """
+    client = ArpegeSimpleAPI(date=date)
+    wind = client.read_wind()
+    
+    return wind.sel(step=pd.Timedelta(step)).si10
+
+def get_sun_map(date: str, step="12h")->xr.DataArray:
+    """Return the mean sun flux at noon for a given date.
+
+    Parameters
+    ----------
+    date : str
+        the date at which the weather forecast is requested.
+        Must be a valid date format, e.g. ``"YYYY-MM-DD"``
+        or a ``datetime.date`` object.
+
+    Returns
+    -------
+    float
+        The mean sun flux at noon.
+
+    Examples
+    --------
+    >>> get_sun_map("2024-06-28")
+    1000.0
+
+    """
+    client = ArpegeSimpleAPI(date=date)
+    sun = client.read_sspd().ssrd.diff("step")
+    #set long_name for the variable
+    sun["long_name"] = "Flux solaire"
+    
+    return sun.sel(step=pd.Timedelta(step))
 if __name__ == "__main__":
 
     st.title("Prévision meteo")
@@ -109,10 +170,51 @@ if __name__ == "__main__":
                 )
 
     sun_data = compute_data_sun(date)
+    sun_data["sun_flux"] = sun_data["sun_flux"] / 1000
     sun_barplot = alt.Chart(sun_data).mark_bar().encode(
-        y=alt.Y("sun_flux:Q").title("Flux solaire (W/m^2)"),
+        y=alt.Y("sun_flux:Q").title("Flux solaire (kW/m²)"),
         x=alt.X("time:T").title("Date")
     ).properties(title="Flux solaire moyen en France.")
     st.altair_chart(sun_barplot,
                     use_container_width=True
                 )
+
+    # hide the section below
+    st.markdown("## Cartes des prévisions météorologiques\n"
+                "Les cartes ci-dessous montrent les prévisions météorologiques pour la France.")
+    with st.expander("Afficher les cartes"):
+        # slider to select the step from 00h00 to 23h00
+        step = st.slider("Sélectionner l'heure de la journée", 0, 23, 12)
+        
+        min_lon, max_lon = france_bounds["min_lon"], france_bounds["max_lon"]
+        min_lat, max_lat = france_bounds["min_lat"], france_bounds["max_lat"]
+        noon_wind = get_noon_wind_map(date, step=f"{step}h").sel(
+            longitude=slice(min_lon, max_lon), latitude=slice(max_lat, min_lat)
+            )
+        
+        fig, ax = plt.subplots(subplot_kw={"projection": ccrs.PlateCarree()})
+        
+        wind_data = noon_wind.plot(ax=ax, transform=ccrs.PlateCarree())
+        #change the colorbar label
+        wind_data.colorbar.set_label("Vitesse du Vent (W/m²)")
+        # zoom on France
+        ax.coastlines()
+        ax.add_feature(cfeature.BORDERS, linestyle=":")
+        ax.add_feature(cfeature.STATES, linestyle=":")
+        ax.set_title(noon_wind.long_name + "\n Le " + f"{pd.Timestamp(noon_wind.valid_time.values):%Y-%m-%d %H:%M}")
+
+        st.pyplot(fig)
+
+        sun_noon = get_sun_map(date, step=f"{step}h").sel(
+            longitude=slice(min_lon, max_lon), latitude=slice(max_lat, min_lat)
+            )
+        fig, ax = plt.subplots(subplot_kw={"projection": ccrs.PlateCarree()})
+        sun_data = sun_noon.plot(ax=ax, transform=ccrs.PlateCarree())
+        #change the colorbar label
+        sun_data.colorbar.set_label("Flux solaire (W/m²)")
+        ax.coastlines()
+        ax.add_feature(cfeature.BORDERS, linestyle=":")
+        ax.add_feature(cfeature.STATES, linestyle=":")
+        ax.set_title("Flux solair\n Le " + f"{pd.Timestamp(sun_noon.valid_time.values):%Y-%m-%d %H:%M}")
+        
+        st.pyplot(fig)
